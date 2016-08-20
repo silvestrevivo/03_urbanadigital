ca3410584bb6605fe11960ce04c3fb71fbb6b3e6
silvestrevivo <silvestrevivomillan@gmail.com>
2016-08-20 10:01:27 +0200

    Start Point
    Structure and Less converted to Sass files

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,5 +1,20 @@
-$(document).ready(function(){
-  $('.jumbotron').click(function(){
-    $('h1').css('color', 'blue');
-  });
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
 });
