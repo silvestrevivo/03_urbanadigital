97bcd9984715f29e6736e483462645ad2ff330fb
silvestrevivo <silvestrevivomillan@gmail.com>
2017-04-24 17:04:31 +0200

    portfolio photography

--- src/js/main.js
+++ src/js/main.js
@@ -28,31 +28,45 @@
         }
     })
 
+    //Slider
+    $('.bxslider1').bxSlider({
+      adaptiveHeight: true,
+      nextText: '>',
+      prevText: '<',
+      pagerCustom: '#bx-pager'
+    });
+    $('.bxslider2').bxSlider({
+      minSlides: 4,
+      maxSlides: 4,
+      slideWidth: 300,
+      slideMargin: 10,
+      ticker: true,
+      speed: 12000
+    });
+
+    //Empty Link Portfolio
+    $('.portfolio-link-last').click(function(e){
+      e.preventDefault();
+    });
+
+    //BounceInUp animation Service Section
+    $(window).scroll(function() {
+      $(".slideanim").each(function(){
+        var pos = $(this).offset().top;
+
+        var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+      });
+    });
+
 })(jQuery); // End of use strict
 
+
 $(document).ready(function(){
-  //Slider
-  $('.bxslider1').bxSlider({
-    adaptiveHeight: true,
-    nextText: '>',
-    prevText: '<',
-    pagerCustom: '#bx-pager'
-  });
-  $('.bxslider2').bxSlider({
-    minSlides: 4,
-    maxSlides: 4,
-    slideWidth: 300,
-    slideMargin: 10,
-    ticker: true,
-    speed: 12000
-  });
-  $('.portfolio-link-last').click(function(e){
-    e.preventDefault();
-  });
-
-
-  //AJAX Requests
-  $('.portfolio-link').click(function(){
+  //Portfolio
+  $('.portfolio-link1').click(function(){
     var a = $(this);
     $.ajax({
       url: '/dist/img/portfolio/projects.json',
@@ -71,17 +85,25 @@ $(document).ready(function(){
       });
     });
   });
-});
-
-//BounceInUp animation
-$(window).scroll(function() {
-  $(".slideanim").each(function(){
-    var pos = $(this).offset().top;
-
-    var winTop = $(window).scrollTop();
-    if (pos < winTop + 600) {
-      //$(this).removeClass('hide');
-      $(this).addClass("slide");
-    }
+  //Photography
+  $('.portfolio-link2').click(function(){
+    var a = $(this);
+    $.ajax({
+      url: '/dist/img/photography/fotos.json',
+      method: 'GET',
+      dataType: 'json',
+    }).then(function(response) {
+      var identification = a.data('id');
+      $.each(response,function(key){
+        var modal = $('#portfolioModal');
+        if(identification === response[key]['id']){
+          modal.find('h2').text(response[key]['title']);
+          modal.find('.item-intro').text(response[key]['subtitle']);
+          modal.find('.img-responsive').attr('src', response[key]['image']);
+          modal.find('.description-project').text(response[key]['body']);
+        }
+      });
+    });
   });
+
 });
